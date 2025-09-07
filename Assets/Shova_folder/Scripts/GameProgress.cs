b1b2039fe17b87a386bb7f64de148e68c969f74b
Shova Awasthi <awasthishova44@gmail.com>
2025-09-07 15:26:39 +1000

    Save & Resume: auto-load + waterhole save

--- Assets/Shova_folder/Scripts/GameProgress.cs
+++ Assets/Shova_folder/Scripts/GameProgress.cs
@@ -2,15 +2,24 @@ using UnityEngine;
 
 public class GameProgress : MonoBehaviour
 {
-    public int unlockedWaterholes = 0;  
+    public int unlockedWaterholes = 0;
     public string lastScene = "MainMenu";
+
+    void Start()
+    {
+        // Auto-load when game starts
+        LoadProgress();
+    }
 
     public void SaveProgress()
     {
+        // Basic sanity check
+        if (unlockedWaterholes < 0) unlockedWaterholes = 0;
+
         PlayerPrefs.SetInt("UnlockedWaterholes", unlockedWaterholes);
         PlayerPrefs.SetString("LastScene", lastScene);
-
         PlayerPrefs.Save();
+
         Debug.Log("Game progress saved!");
     }
 
@@ -34,6 +43,17 @@ public class GameProgress : MonoBehaviour
     public void ResetProgress()
     {
         PlayerPrefs.DeleteAll();
+        unlockedWaterholes = 0;
+        lastScene = "MainMenu";
         Debug.Log("Game progress reset!");
+    }
+
+    void Update()
+    {
+        // Optional: press R to reset during testing
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ResetProgress();
+        }
     }
 }
